7c24ce8e44f48e1e6d5a118c1de7030587e46fc3
Aluisali <analuisalidogario@gmail.com>
2025-05-13 21:07:44 -0300

    Add files via upload

---
+++ cardapio_lanche.py
@@ -0,0 +1,24 @@
+opcao =0
+while opcao !=5:
+    print ("---- CARDÁPIO----")
+    print ("1- Hambúrguer")
+    print ("2- Pizza")
+    print ("3- Salada")
+    print ("4- Refrigerante")
+    print ("5- Sair")
+    opcao = int(input("Escolha uma opção: "))
+    if opcao ==1:
+        print ("Você escolheu Hambúrguer")
+    elif opcao ==2:
+        print ("Você escolheu Pizza")
+    elif opcao ==3:
+        print ("Você esvolheu Salada")
+    elif opcao ==4:
+        print ("Você escolheu Refrigente")
+    elif opcao ==5:
+        print ("Saindo do cardápio.")
+        break 
+
+
+    
+ 
